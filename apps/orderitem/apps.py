8b5725a716bc2afd49b63cfeac2719015a506268
João Augusto da Veiga Carbonera <134736365+JoaoCarbonera@users.noreply.github.com>
2025-04-29 20:19:52 -0300

    Started App

---
+++ apps/orderitem/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class OrderitemConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'orderitem'
+    verbose_name = 'Itens de pedido'
