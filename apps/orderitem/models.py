f34eb2fd1fcaac0f9dd66545e509483fb61f3144
João Augusto da Veiga Carbonera <134736365+JoaoCarbonera@users.noreply.github.com>
2025-04-29 20:20:54 -0300

    Models.py Finalizado

--- apps/orderitem/models.py
+++ apps/orderitem/models.py
@@ -1,5 +1,19 @@
 from django.db import models
-
+from products.models import Product
+from clients.models import Client
 
 # Create your models here.
 
+class OrderItem(models.Model):
+    quantity = models.IntegerField('Quantidade',null=True, blank=True,default=0)
+    unitary_price = models.FloatField('Preco unitario',null=True, blank=True, default=0.0)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Item de pedido'
+        verbose_name_plural = 'Itens de pedido'
+        ordering =['id']
+
+    def __str__(self):
+        return "%s" % (self.quantity) 
