30af15ffee7a8ab062dcc39712079e39a154cc95
xXDoenixXx <122305108+xXDoenixXx@users.noreply.github.com>
2025-04-29 19:56:07 -0300

    Merge?

--- modeloapp/urls.py
+++ modeloapp/urls.py
@@ -24,4 +24,6 @@ urlpatterns = [
     path('redessocias/', include('socialnetworks.urls', namespace='redessociais')), 
     path('clientes/', include('clients.urls', namespace='clients')),
     path('clientes_redessociais/', include('clients.urls', namespace='clients_socialnetworks')),  
+    path('pedidos/', include('orders.urls', namespace='orders')),
+    path('pedidos_itens/', include('orders.urls', namespace='orders_items')),
 ]
